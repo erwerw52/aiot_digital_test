c863fa0ae509b81e31b19d2566e3027d4276f97d
erwerw52 <erwerw52@gmail.com>
2025-12-06 12:32:32 +0800

    feat: 新增數位人類頁面及資源檔案
    - 更新 pubspec.yaml，新增數位人類模型資源檔案
    - 新增 DigitalHumanPage 頁面以展示數位人類功能

---
+++ lib/digital/digital_human_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DigitalHumanPage extends StatefulWidget {
+  const DigitalHumanPage({super.key});
+
+  @override
+  State<DigitalHumanPage> createState() => _DigitalHumanPageState();
+}
+
+class _DigitalHumanPageState extends State<DigitalHumanPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
